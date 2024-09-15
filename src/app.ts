4f848f1aece7dcf0886cf86310d2ec4c9cc03306
arthur <arthur.rios007@gmail.com>
2024-09-15 11:14:24 -0300

    add prisma config

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Arthur Rios',
+    email: 'arthur.rios007@gmail.com',
+  },
+})
